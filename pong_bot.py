354a2ec392e7559d33839673573f9824eff8fb43
jenzsun <jennifersun19@gmail.com>
2020-09-08 21:38:04 -0700

    Made Paddle object.

--- pong_bot.py
+++ pong_bot.py
@@ -8,12 +8,36 @@ window = pygame.display.set_mode((750, 500))
 
 pygame.display.set_caption('Pong')
 
+white = (255, 255, 255)
+black = (0, 0, 0)
+
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface([10, 75])
+        self.image.fill(white)
+        self.rect = self.image.get_rect()
+        self.points = 0
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface([10, 10])
+        self.image.fill(white)
+        self.rect = self.image.get_rect()
+
 run = True
 
 while run:
+    pygame.time.delay(100)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+            continue
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                run = False
+                continue
 
 pygame.quit()
